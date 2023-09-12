aa4764df00ff735fcfb8877965b3daabd75b8a6e
queenyxies <queenycasaclang@gmail.com>
2023-09-13 02:06:18 +0800

    DB connect reservation

--- reserved.php
+++ reserved.php
@@ -14,7 +14,7 @@ $result = $conn -> query($sql);
 <head>
     <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
     <title>Reservation</title>
-    <!------------------------ Bootstrap 4 ------------------------>
+    <!------------------------ Bootstrap 4 ------------------------->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
