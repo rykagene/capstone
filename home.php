ff1ba318d5778bd736cf885b4dd5df7524b60fc7
Ryka Gene Austria <82452004+rykagene@users.noreply.github.com>
2023-09-27 10:14:04 +0800

    Update home.php added #clock

--- home.php
+++ home.php
@@ -97,6 +97,19 @@
             </a>
         </div>
 
+        <div id="clock">
+            <div class="clock">
+                <div class="hour">
+                    <div class="hr" id="hr"></div>
+                </div>
+                <div class="min">
+                    <div class="mn" id="mn"></div>
+                </div>
+                <div class="sec">
+                    <div class="sc" id="sc"></div>
+                </div>
+            </div>
+        </div>
 
         <!------------------------ END OF SEAT INFO ------------------------>
 
