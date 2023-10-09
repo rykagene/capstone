1d62cf5339d479ecafb77b94d9eae579483d7571
Jeays Digo <jeaysdigo@outlook.com>
2023-10-09 22:07:51 +0800

    inserted year and section

--- soar.sql
+++ soar.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 09, 2023 at 08:20 AM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Generation Time: Oct 09, 2023 at 04:05 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,24 +35,7 @@ CREATE TABLE `account` (
   `picture` varchar(255) DEFAULT NULL,
   `account_type` varchar(255) DEFAULT NULL,
   `reservation_count` int(11) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `account`
---
-
-INSERT INTO `account` (`account_id`, `username`, `password`, `email`, `picture`, `account_type`, `reservation_count`) VALUES
-(1, 'ricardojeyms', '123', 'richard@soar.com', NULL, 'admin', 0),
-(2, '2020103475', '123', 'jeaysmie.digo.m@bulsu.edu.ph', 'assets/img/profilejeays id picture bsu.jpg', 'student', 0),
-(3, '1234', '1234', 'digo.jeaysmie.m.3475@gmail.com', NULL, 'student', 0),
-(4, '123', '123', '123@gmail.com', NULL, 'student', 0),
-(5, 'nosection', '123', 'nosection@ga.com', NULL, 'student', 0),
-(6, '123456', '123', '123@gmail.com', NULL, 'student', 0),
-(7, '1234567', '123456', '123@gmail.com', NULL, 'student', 0),
-(8, 'mj23', '123', 'mj23@gmail.com', NULL, 'admin', 0),
-(9, 'robertooo', '123', 'robert@gmail.com', NULL, 'admin', 0),
-(10, '3030', '$2y$10$yPh9WR/txTZO6uNGjCS01.i2UKWNy8dFHghb4Y1NXF3iHRUsZkeOO', 'tifa@gmail.com', NULL, 'admin', 0),
-(11, '7777', '$2y$10$HM2ajDWHl.gxA/CKDxiKcOjl4K5zh97UwA1eVtEaAqwNRiQO/N68G', 'cloud@gmail.com', NULL, 'student', 0);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -75,7 +58,7 @@ CREATE TABLE `admin` (
   `home_address` varchar(255) DEFAULT NULL,
   `work_status` varchar(20) DEFAULT NULL,
   `account_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admin`
@@ -95,7 +78,7 @@ INSERT INTO `admin` (`admin_id`, `isSuperAdmin`, `rfid_no`, `department`, `first
 CREATE TABLE `college` (
   `college_code` varchar(50) NOT NULL,
   `college_name` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `college`
@@ -114,14 +97,17 @@ CREATE TABLE `course` (
   `course_code` varchar(50) NOT NULL,
   `course_name` varchar(255) DEFAULT NULL,
   `college_code` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `course`
 --
 
 INSERT INTO `course` (`course_code`, `course_name`, `college_code`) VALUES
-('BSIT', 'Bachelor of Science in Information Technology', 'CICT');
+('Alumni', 'Alumni', 'Alumni'),
+('BLIS', 'Bachelor of Library and Information Science', 'CICT'),
+('BSIT', 'Bachelor of Science in Information Technology', 'CICT'),
+('Faculty', 'Faculty', 'Faculty');
 
 -- --------------------------------------------------------
 
@@ -138,18 +124,7 @@ CREATE TABLE `history` (
   `user_id` int(11) DEFAULT NULL,
   `seat_id` int(11) DEFAULT NULL,
   `time_spent` time DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `history`
---
-
-INSERT INTO `history` (`history_id`, `reservation_id`, `date`, `start_time`, `end_time`, `user_id`, `seat_id`, `time_spent`) VALUES
-(34, 87, '2023-09-27', '11:01:52', '11:06:12', 2020103475, 2, '00:04:20'),
-(35, 87, '2023-09-27', '11:01:52', '11:06:13', 2020103475, 2, '00:04:20'),
-(36, 88, '2023-09-28', '00:00:00', '20:17:59', 123, 1, '00:00:00'),
-(37, 89, '2023-10-09', '00:00:00', '23:26:13', 7777, 2, '00:00:00'),
-(38, 90, '2023-10-24', '00:00:00', '17:44:51', 7777, 1, '00:00:00');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -162,7 +137,7 @@ CREATE TABLE `news` (
   `picture` varchar(255) DEFAULT NULL,
   `content` text DEFAULT NULL,
   `date` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -180,14 +155,7 @@ CREATE TABLE `occupy` (
   `seat_id` int(11) DEFAULT NULL,
   `time_spent` time DEFAULT NULL,
   `isDone` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `occupy`
---
-
-INSERT INTO `occupy` (`occupy_id`, `reservation_id`, `date`, `start_time`, `end_time`, `user_id`, `seat_id`, `time_spent`, `isDone`) VALUES
-(88, 87, '2023-09-27', '11:01:52', '11:06:13', 2020103475, 2, '00:04:20', 1);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -201,19 +169,7 @@ CREATE TABLE `rating` (
   `review` varchar(255) DEFAULT NULL,
   `date` date DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `rating`
---
-
-INSERT INTO `rating` (`rating_id`, `rating`, `review`, `date`, `user_id`) VALUES
-(1, 5, 'sheesh', '2023-09-19', 2020103475),
-(2, 3, 'nice!', '2023-09-19', 2020103475),
-(3, 5, 'nice!', '2023-09-19', 2020103475),
-(4, 4, '', '2023-09-19', 2020103475),
-(5, 3, '', '2023-09-27', 2020103475),
-(6, 4, 'sfdgdfdsfsd', '2023-09-27', 2020103475);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -229,17 +185,7 @@ CREATE TABLE `reservation` (
   `user_id` int(11) DEFAULT NULL,
   `seat_id` int(11) DEFAULT NULL,
   `isDone` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `reservation`
---
-
-INSERT INTO `reservation` (`reservation_id`, `date`, `start_time`, `end_time`, `user_id`, `seat_id`, `isDone`) VALUES
-(87, '2023-09-27', '10:47:00', '11:06:00', 2020103475, 2, 1),
-(88, '2023-09-28', '10:17:00', '12:17:00', 123, 1, 1),
-(89, '2023-10-09', '15:57:00', '16:57:00', 7777, 2, 1),
-(90, '2023-10-24', '11:45:00', '12:45:00', 7777, 1, 1);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -252,7 +198,7 @@ CREATE TABLE `seat` (
   `seat_number` varchar(50) DEFAULT NULL,
   `data_surface` varchar(255) NOT NULL,
   `status` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `seat`
@@ -280,7 +226,7 @@ CREATE TABLE `settings` (
   `start_hour` time NOT NULL,
   `end_hour` time NOT NULL,
   `disabled_dates` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `settings`
@@ -305,22 +251,7 @@ CREATE TABLE `users` (
   `yearsec_id` int(11) DEFAULT NULL,
   `age` int(11) DEFAULT NULL,
   `contact_number` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`user_id`, `rfid_no`, `first_name`, `last_name`, `account_id`, `course_code`, `yearsec_id`, `age`, `contact_number`) VALUES
-(0, NULL, 'nosection', 'nosection', 5, NULL, NULL, NULL, NULL),
-(123, NULL, 'noCourse', 'noCourse', 4, NULL, 9, NULL, NULL),
-(1234, NULL, 'JEAYSMIE', 'DIGO', 3, 'BSIT', 9, NULL, NULL),
-(3030, NULL, 'Tifa', 'Lockhart', 10, NULL, NULL, NULL, NULL),
-(7777, NULL, 'Cloud', 'Strife', 11, NULL, NULL, NULL, NULL),
-(123456, NULL, '1234', '1234', 6, NULL, NULL, NULL, NULL),
-(1234567, NULL, 'noCourses', 'noCourses', 7, NULL, NULL, NULL, NULL),
-(2020103475, '12345', 'Jeaysmie', 'Digo', 2, 'BSIT', 9, NULL, NULL),
-(2023000001, NULL, 'admin', 'admin', 1, NULL, NULL, NULL, NULL);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -333,65 +264,274 @@ CREATE TABLE `yearsec` (
   `year_level` int(11) DEFAULT NULL,
   `section` varchar(10) DEFAULT NULL,
   `section_group` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `yearsec`
 --
 
 INSERT INTO `yearsec` (`yearsec_id`, `year_level`, `section`, `section_group`) VALUES
-(1, 3, 'A', '1'),
-(2, 3, 'A', '2'),
-(3, 3, 'B', '1'),
-(4, 3, 'B', '2'),
-(5, 3, 'C', '1'),
-(6, 3, 'C', '2'),
-(7, 3, 'D', '1'),
-(8, 3, 'D', '2'),
-(9, 3, 'E', '1'),
-(10, 3, 'E', '2'),
-(11, 3, 'F', '1'),
-(12, 3, 'F', '2'),
-(13, 3, 'G', '1'),
-(14, 3, 'G', '2'),
-(15, 3, 'H', '1'),
-(16, 3, 'H', '2'),
-(17, 3, 'I', '1'),
-(18, 3, 'I', '2'),
-(19, 3, 'J', '1'),
-(20, 3, 'J', '2'),
-(21, 3, 'K', '1'),
-(22, 3, 'K', '2'),
-(23, 3, 'L', '1'),
-(24, 3, 'L', '2'),
-(25, 3, 'M', '1'),
-(26, 3, 'M', '2'),
-(27, 3, 'N', '1'),
-(28, 3, 'N', '2'),
-(29, 3, 'O', '1'),
-(30, 3, 'O', '2'),
-(31, 3, 'P', '1'),
-(32, 3, 'P', '2'),
-(33, 3, 'Q', '1'),
-(34, 3, 'Q', '2'),
-(35, 3, 'R', '1'),
-(36, 3, 'R', '2'),
-(37, 3, 'S', '1'),
-(38, 3, 'S', '2'),
-(39, 3, 'T', '1'),
-(40, 3, 'T', '2'),
-(41, 3, 'U', '1'),
-(42, 3, 'U', '2'),
-(43, 3, 'V', '1'),
-(44, 3, 'V', '2'),
-(45, 3, 'W', '1'),
-(46, 3, 'W', '2'),
-(47, 3, 'X', '1'),
-(48, 3, 'X', '2'),
-(49, 3, 'Y', '1'),
-(50, 3, 'Y', '2'),
-(51, 3, 'Z', '1'),
-(52, 3, 'Z', '2');
+(1, 1, 'A', '1'),
+(2, 1, 'A', '2'),
+(3, 1, 'B', '1'),
+(4, 1, 'B', '2'),
+(5, 1, 'C', '1'),
+(6, 1, 'C', '2'),
+(7, 1, 'D', '1'),
+(8, 1, 'D', '2'),
+(9, 1, 'E', '1'),
+(10, 1, 'E', '2'),
+(11, 1, 'F', '1'),
+(12, 1, 'F', '2'),
+(13, 1, 'G', '1'),
+(14, 1, 'G', '2'),
+(15, 1, 'H', '1'),
+(16, 1, 'H', '2'),
+(17, 1, 'I', '1'),
+(18, 1, 'I', '2'),
+(19, 1, 'J', '1'),
+(20, 1, 'J', '2'),
+(21, 1, 'K', '1'),
+(22, 1, 'K', '2'),
+(23, 1, 'L', '1'),
+(24, 1, 'L', '2'),
+(25, 1, 'M', '1'),
+(26, 1, 'M', '2'),
+(27, 1, 'N', '1'),
+(28, 1, 'N', '2'),
+(29, 1, 'O', '1'),
+(30, 1, 'O', '2'),
+(31, 1, 'P', '1'),
+(32, 1, 'P', '2'),
+(33, 1, 'Q', '1'),
+(34, 1, 'Q', '2'),
+(35, 1, 'R', '1'),
+(36, 1, 'R', '2'),
+(37, 1, 'S', '1'),
+(38, 1, 'S', '2'),
+(39, 1, 'T', '1'),
+(40, 1, 'T', '2'),
+(41, 1, 'U', '1'),
+(42, 1, 'U', '2'),
+(43, 1, 'V', '1'),
+(44, 1, 'V', '2'),
+(45, 1, 'W', '1'),
+(46, 1, 'W', '2'),
+(47, 1, 'X', '1'),
+(48, 1, 'X', '2'),
+(49, 1, 'Y', '1'),
+(50, 1, 'Y', '2'),
+(51, 1, 'Z', '1'),
+(52, 1, 'Z', '2'),
+(53, 2, 'A', '1'),
+(54, 2, 'A', '2'),
+(55, 2, 'B', '1'),
+(56, 2, 'B', '2'),
+(57, 2, 'C', '1'),
+(58, 2, 'C', '2'),
+(59, 2, 'D', '1'),
+(60, 2, 'D', '2'),
+(61, 2, 'E', '1'),
+(62, 2, 'E', '2'),
+(63, 2, 'F', '1'),
+(64, 2, 'F', '2'),
+(65, 2, 'G', '1'),
+(66, 2, 'G', '2'),
+(67, 2, 'H', '1'),
+(68, 2, 'H', '2'),
+(69, 2, 'I', '1'),
+(70, 2, 'I', '2'),
+(71, 2, 'J', '1'),
+(72, 2, 'J', '2'),
+(73, 2, 'K', '1'),
+(74, 2, 'K', '2'),
+(75, 2, 'L', '1'),
+(76, 2, 'L', '2'),
+(77, 2, 'M', '1'),
+(78, 2, 'M', '2'),
+(79, 2, 'N', '1'),
+(80, 2, 'N', '2'),
+(81, 2, 'O', '1'),
+(82, 2, 'O', '2'),
+(83, 2, 'P', '1'),
+(84, 2, 'P', '2'),
+(85, 2, 'Q', '1'),
+(86, 2, 'Q', '2'),
+(87, 2, 'R', '1'),
+(88, 2, 'R', '2'),
+(89, 2, 'S', '1'),
+(90, 2, 'S', '2'),
+(91, 2, 'T', '1'),
+(92, 2, 'T', '2'),
+(93, 2, 'U', '1'),
+(94, 2, 'U', '2'),
+(95, 2, 'V', '1'),
+(96, 2, 'V', '2'),
+(97, 2, 'W', '1'),
+(98, 2, 'W', '2'),
+(99, 2, 'X', '1'),
+(100, 2, 'X', '2'),
+(101, 2, 'Y', '1'),
+(102, 2, 'Y', '2'),
+(103, 2, 'Z', '1'),
+(104, 2, 'Z', '2'),
+(105, 3, 'A', '1'),
+(106, 3, 'A', '2'),
+(107, 3, 'B', '1'),
+(108, 3, 'B', '2'),
+(109, 3, 'C', '1'),
+(110, 3, 'C', '2'),
+(111, 3, 'D', '1'),
+(112, 3, 'D', '2'),
+(113, 3, 'E', '1'),
+(114, 3, 'E', '2'),
+(115, 3, 'F', '1'),
+(116, 3, 'F', '2'),
+(117, 3, 'G', '1'),
+(118, 3, 'G', '2'),
+(119, 3, 'H', '1'),
+(120, 3, 'H', '2'),
+(121, 3, 'I', '1'),
+(122, 3, 'I', '2'),
+(123, 3, 'J', '1'),
+(124, 3, 'J', '2'),
+(125, 3, 'K', '1'),
+(126, 3, 'K', '2'),
+(127, 3, 'L', '1'),
+(128, 3, 'L', '2'),
+(129, 3, 'M', '1'),
+(130, 3, 'M', '2'),
+(131, 3, 'N', '1'),
+(132, 3, 'N', '2'),
+(133, 3, 'O', '1'),
+(134, 3, 'O', '2'),
+(135, 3, 'P', '1'),
+(136, 3, 'P', '2'),
+(137, 3, 'Q', '1'),
+(138, 3, 'Q', '2'),
+(139, 3, 'R', '1'),
+(140, 3, 'R', '2'),
+(141, 3, 'S', '1'),
+(142, 3, 'S', '2'),
+(143, 3, 'T', '1'),
+(144, 3, 'T', '2'),
+(145, 3, 'U', '1'),
+(146, 3, 'U', '2'),
+(147, 3, 'V', '1'),
+(148, 3, 'V', '2'),
+(149, 3, 'W', '1'),
+(150, 3, 'W', '2'),
+(151, 3, 'X', '1'),
+(152, 3, 'X', '2'),
+(153, 3, 'Y', '1'),
+(154, 3, 'Y', '2'),
+(155, 3, 'Z', '1'),
+(156, 3, 'Z', '2'),
+(157, 4, 'A', '1'),
+(158, 4, 'A', '2'),
+(159, 4, 'B', '1'),
+(160, 4, 'B', '2'),
+(161, 4, 'C', '1'),
+(162, 4, 'C', '2'),
+(163, 4, 'D', '1'),
+(164, 4, 'D', '2'),
+(165, 4, 'E', '1'),
+(166, 4, 'E', '2'),
+(167, 4, 'F', '1'),
+(168, 4, 'F', '2'),
+(169, 4, 'G', '1'),
+(170, 4, 'G', '2'),
+(171, 4, 'H', '1'),
+(172, 4, 'H', '2'),
+(173, 4, 'I', '1'),
+(174, 4, 'I', '2'),
+(175, 4, 'J', '1'),
+(176, 4, 'J', '2'),
+(177, 4, 'K', '1'),
+(178, 4, 'K', '2'),
+(179, 4, 'L', '1'),
+(180, 4, 'L', '2'),
+(181, 4, 'M', '1'),
+(182, 4, 'M', '2'),
+(183, 4, 'N', '1'),
+(184, 4, 'N', '2'),
+(185, 4, 'O', '1'),
+(186, 4, 'O', '2'),
+(187, 4, 'P', '1'),
+(188, 4, 'P', '2'),
+(189, 4, 'Q', '1'),
+(190, 4, 'Q', '2'),
+(191, 4, 'R', '1'),
+(192, 4, 'R', '2'),
+(193, 4, 'S', '1'),
+(194, 4, 'S', '2'),
+(195, 4, 'T', '1'),
+(196, 4, 'T', '2'),
+(197, 4, 'U', '1'),
+(198, 4, 'U', '2'),
+(199, 4, 'V', '1'),
+(200, 4, 'V', '2'),
+(201, 4, 'W', '1'),
+(202, 4, 'W', '2'),
+(203, 4, 'X', '1'),
+(204, 4, 'X', '2'),
+(205, 4, 'Y', '1'),
+(206, 4, 'Y', '2'),
+(207, 4, 'Z', '1'),
+(208, 4, 'Z', '2'),
+(209, 5, 'A', '1'),
+(210, 5, 'A', '2'),
+(211, 5, 'B', '1'),
+(212, 5, 'B', '2'),
+(213, 5, 'C', '1'),
+(214, 5, 'C', '2'),
+(215, 5, 'D', '1'),
+(216, 5, 'D', '2'),
+(217, 5, 'E', '1'),
+(218, 5, 'E', '2'),
+(219, 5, 'F', '1'),
+(220, 5, 'F', '2'),
+(221, 5, 'G', '1'),
+(222, 5, 'G', '2'),
+(223, 5, 'H', '1'),
+(224, 5, 'H', '2'),
+(225, 5, 'I', '1'),
+(226, 5, 'I', '2'),
+(227, 5, 'J', '1'),
+(228, 5, 'J', '2'),
+(229, 5, 'K', '1'),
+(230, 5, 'K', '2'),
+(231, 5, 'L', '1'),
+(232, 5, 'L', '2'),
+(233, 5, 'M', '1'),
+(234, 5, 'M', '2'),
+(235, 5, 'N', '1'),
+(236, 5, 'N', '2'),
+(237, 5, 'O', '1'),
+(238, 5, 'O', '2'),
+(239, 5, 'P', '1'),
+(240, 5, 'P', '2'),
+(241, 5, 'Q', '1'),
+(242, 5, 'Q', '2'),
+(243, 5, 'R', '1'),
+(244, 5, 'R', '2'),
+(245, 5, 'S', '1'),
+(246, 5, 'S', '2'),
+(247, 5, 'T', '1'),
+(248, 5, 'T', '2'),
+(249, 5, 'U', '1'),
+(250, 5, 'U', '2'),
+(251, 5, 'V', '1'),
+(252, 5, 'V', '2'),
+(253, 5, 'W', '1'),
+(254, 5, 'W', '2'),
+(255, 5, 'X', '1'),
+(256, 5, 'X', '2'),
+(257, 5, 'Y', '1'),
+(258, 5, 'Y', '2'),
+(259, 5, 'Z', '1'),
+(260, 5, 'Z', '2'),
+(261, 0, 'NONE', 'NONE');
 
 --
 -- Indexes for dumped tables
@@ -497,7 +637,7 @@ ALTER TABLE `yearsec`
 -- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
-  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `history`
