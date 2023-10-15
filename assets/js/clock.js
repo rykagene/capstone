2fc6b4b354c981d204ad147bc8bd1486b893c2cb
Jeays Digo <jeaysdigo@outlook.com>
2023-10-16 01:45:13 +0800

    get manila time using api

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,22 +1,28 @@
 function updateTime() {
     const clock = document.getElementById('clock');
-    const now = new Date();
-    let hours = now.getHours();
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-    let timeOfDay = 'AM';
-
-    if (hours >= 12) {
-        timeOfDay = 'PM';
-        if (hours > 12) {
-            hours -= 12;
+    
+    // Make an AJAX request to get the server's time
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'get_server_time.php', true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            const serverTime = new Date(response.server_time);
+            
+            const options = {
+                timeZone: 'Asia/Manila',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+                hour12: true
+            };
+            
+            const timeString = serverTime.toLocaleTimeString('en-US', options);
+            
+            clock.textContent = timeString;
         }
-    }
-
-    hours = hours.toString().padStart(2, '0');
-    const timeString = `${hours}:${minutes}:${seconds} ${timeOfDay}`;
-
-    clock.textContent = timeString;
+    };
+    xhr.send();
 }
 
 setInterval(updateTime, 1000);
