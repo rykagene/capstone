523a64aec03f5067a7931ee7c060e7009476ace5
Ryka Gene Austria <82452004+rykagene@users.noreply.github.com>
2023-10-29 19:27:55 +0800

    added adminReviews.js

---
+++ assets/js/adminReviews.js
@@ -0,0 +1,13 @@
+
+const progressDone = document.querySelectorAll('.progress-done');
+
+progressDone.forEach(progress => {
+	progress.style.width = progress.getAttribute('data-done') + '%';
+});
+
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
